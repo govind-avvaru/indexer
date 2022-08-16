de8e2a1461184c70756fb612d17a9ced26b5dd84
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2022-08-16 20:47:13 +0530

    Update Test.php

--- CustomIndexer/Model/Indexer/Test.php
+++ CustomIndexer/Model/Indexer/Test.php
@@ -7,49 +7,37 @@ use Magento\Framework\Mview\ActionInterface as MviewInterface;
 class Test implements IndexerInterface, MviewInterface
 {
 
-    /**
-     * It's used by mview. It will execute when process indexer in "Update on schedule" Mode.
-     */
-    public function execute($ids)
-    {
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info('Execute Method Call When Update on Schedule');
-    }
+    /*
+	 * Used by mview, allows process indexer in the "Update on schedule" mode
+	 */
+	public function execute($ids){
 
-    /**
-     * Add code here for execute full indexation
-     */
-    public function executeFull()
-    {
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info('Execute Full Method Call When Re index using command line');
-    }
+		//code here!
+	}
 
-    /**
-     * Add code here for execute partial indexation by ID list
-     */
-    public function executeList(array $ids)
-    {
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info('Execute List Method Call When partial indextion by id list');
-    }
-
-    /**
-     * Add code here for execute partial indexation by ID
-     */
-    public function executeRow($id)
-    {
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info('Execute Row Method Call When partial indextion by specific id');
-    }
+	/*
+	 * Will take all of the data and reindex
+	 * Will run when reindex via command line
+	 */
+	public function executeFull(){
+		//code here!
+	}
+   
+   
+	/*
+	 * Works with a set of entity changed (may be massaction)
+	 */
+	public function executeList(array $ids){
+		//code here!
+	}
+   
+   
+	/*
+	 * Works in runtime for a single entity using plugins
+	 */
+	public function executeRow($id){
+		//code here!
+	}
 }
 
 
